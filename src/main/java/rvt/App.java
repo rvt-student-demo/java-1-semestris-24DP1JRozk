0b986b11c3bcea510f870323f48f53add9f7e35e
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-09-18 12:26:50 +0300

    Even or uneven number, java mooc 1 6

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -5,30 +5,17 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Course grade grader!\n");
+        System.out.println("Even or uneven numbers!\n");
 
-        System.out.println("Input your course grade:");
-        double courseGrade = Integer.valueOf(scanner.nextLine());
+        System.out.println("Input number:");
+        double num = Integer.valueOf(scanner.nextLine());
 
         scanner.close();
-        System.out.println("Your course grade is:");
 
-        if (courseGrade < 0){
-            System.out.println("Error! Impossible!");
-        } else if (courseGrade <= 49) {
-            System.out.println("Failed!");
-        } else if (courseGrade <= 59) {
-            System.out.println("1");
-        } else if (courseGrade <= 69) {
-            System.out.println("2");
-        } else if (courseGrade <= 79) {
-            System.out.println("3");
-        } else if (courseGrade <= 89) {
-            System.out.println("4");
-        } else if (courseGrade <= 100) {
-            System.out.println("5");
-        } else if (courseGrade > 100) {
-            System.out.println("Incredible!");
+        if (num % 2 == 1) {
+            System.out.println("Number is uneven!");
+        } else {
+            System.out.println("Number is even!");
         }
     }
 }
