d7a1e6de01f3b5972538c715778b25a19a39dc18
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-09-24 08:58:14 +0300

    Gift tax exercise

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -5,17 +5,25 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Even or uneven numbers!\n");
+        System.out.println("Gift tax!\n");
 
-        System.out.println("Input number:");
+        System.out.println("Input money value of gifts got in past years:");
         double num = Integer.valueOf(scanner.nextLine());
 
         scanner.close();
 
-        if (num % 2 == 1) {
-            System.out.println("Number is uneven!");
+        if (num < 5000) {
+            System.out.println("No tax!");
+        } else if (num < 25000) {
+            System.out.println("Gift tax to pay: " + (100 + (num - 5000) * 0.08));
+        } else if (num < 55000) {
+            System.out.println("Gift tax to pay: " + (1700 + (num - 25000) * 0.1));
+        } else if (num < 200000) {
+            System.out.println("Gift tax to pay: " + (4700 + (num - 55000) * 0.12));
+        } else if (num < 1000000) {
+            System.out.println("Gift tax to pay: " + (22100 + (num - 200000) * 0.15));
         } else {
-            System.out.println("Number is even!");
+            System.out.println("Gift tax to pay: " + (142100 + (num - 1000000) * 0.17));
         }
     }
 }
