ea1f3a5a95f8b33b6af41f77f0313c19e0173526
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-09-24 09:08:41 +0300

    Skruves, uzgriezni, saplaksni

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,29 +1,26 @@
 package rvt;
+
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Gift tax!\n");
+        System.out.println("Skrūves, Uzgriežņi, Paplākšņi!\n");
 
-        System.out.println("Input money value of gifts got in past years:");
-        double num = Integer.valueOf(scanner.nextLine());
+        System.out.println("Ievadi skrūvju daudzumu:");
+        double skruves = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Ievadi uzgriežņu daudzumu:");
+        double uzgriezni = Integer.valueOf(scanner.nextLine());
+
+        System.out.println("Ievadi skrūvju daudzumu:");
+        double sapklaksni = Integer.valueOf(scanner.nextLine());
 
         scanner.close();
 
-        if (num < 5000) {
-            System.out.println("No tax!");
-        } else if (num < 25000) {
-            System.out.println("Gift tax to pay: " + (100 + (num - 5000) * 0.08));
-        } else if (num < 55000) {
-            System.out.println("Gift tax to pay: " + (1700 + (num - 25000) * 0.1));
-        } else if (num < 200000) {
-            System.out.println("Gift tax to pay: " + (4700 + (num - 55000) * 0.12));
-        } else if (num < 1000000) {
-            System.out.println("Gift tax to pay: " + (22100 + (num - 200000) * 0.15));
-        } else {
-            System.out.println("Gift tax to pay: " + (142100 + (num - 1000000) * 0.17));
-        }
+        final double totalPrice = skruves * 0.05 + uzgriezni * 0.03 + sapklaksni * 0.01;
+
+        System.out.println("Kopējā summa: " + totalPrice + "€");
     }
 }
