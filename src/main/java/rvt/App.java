484fc0d2ce85ed571ce9448aebab24ad50ad1940
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-09-26 09:53:48 +0300

    Inputed numbers, does calculations, 2-3

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -8,26 +8,38 @@ public class App {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Input numbers, to stop, input '0':");
+        System.out.println("Input numbers, to stop, input '-1':");
 
         int sum = 0;
         int count = 0;
+        int even = 0;
+        int odd = 0;
 
         while (true) {
             int number = Integer.valueOf(scanner.nextLine());
 
-            count ++;
-
-            if (number == 0) {
+            if (number == -1) {
                 break;
             }
 
+            if (number % 2 == 1) {
+                odd ++;
+            } else {
+                even ++;
+            }
+
             sum = sum + number;
+
+            count ++;
         }
 
         scanner.close();
 
-        System.out.println("Numbers inputed: " + count);
+        System.out.println("Thx! Bye!");
         System.out.println("Sum of inputed numbers: " + sum);
+        System.out.println("Numbers inputed: " + count);
+        System.out.println("Average: " + (1.0 * sum / count));
+        System.out.println("Even number count: " + even);
+        System.out.println("Odd number count: " + odd);
     }
 }
