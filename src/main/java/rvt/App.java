add812e4f6fcd2147bdda937ea6554f7aa615781
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-09-26 09:36:05 +0300

    Input number, program counts from 0 to that number

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -4,34 +4,20 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Counter!\n");
+
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Sam & Ella delikateses!\n");
-
-        System.out.println("Ievadi preces nosaukumu:");
-        String name = scanner.nextLine();
-
-        System.out.println("Ievadi cenu:");
-        double price = Double.valueOf(scanner.nextLine());
-
-        System.out.println("Vai bus nepieciešana express piegade? [1] = ja  [2] = ne:");
-        int express = Integer.valueOf(scanner.nextLine());
-
+        System.out.println("Input number:");
+        int number = Integer.valueOf(scanner.nextLine());
         scanner.close();
 
-        double expressPrice = 0;
-        if (express == 1) {
-            expressPrice = 3;
-        }
+        System.out.println("");
 
-        double piegade = 2;
-        if (price > 10) {
-            piegade = 5;
-        }
 
-        System.out.println("Rēķins:");
-        System.out.println(name + "   " + price);
-        System.out.println("Piegade:   " + (piegade + expressPrice));
-        System.out.println("Kopa:   " + (price + piegade + expressPrice));
+        for (int i = 0; i <= number; i ++) {
+            System.out.println(i);
+        }
+        System.out.println("Done!");
     }
 }
