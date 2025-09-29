52996326d44b3f9729ceabda4ebfc89cd3413c01
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-09-29 12:25:25 +0300

    Program that counts from start var to end var

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -2,46 +2,21 @@ package rvt;
 
 import java.util.Scanner;
 
-import java.util.Random;
-
 public class App {
+    public static void divisibleByThree(int startV, int endV) {
+        System.out.println();
+        for (int i = startV; i < endV + 1; i++) {
+            System.out.println(i);
+        }      
+    }
+
     public static void main(String[] args) {
-
-        System.out.println("Guesser!");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Whats the max number that the game can think of?");
-        int maxNumber = Integer.valueOf(scanner.nextLine());
-        Random random = new Random();
-        int result = random.nextInt(maxNumber) + 1;
-        System.out.println("Guess the number from 0 to " + maxNumber + "!");
 
-        int tries = 0;
-
-        while (true) {
-            int guess = Integer.valueOf(scanner.nextLine());
-            tries ++;
-            if (guess == result) {
-                System.out.println("Correct!");
-                break;
-            } else {
-                System.out.println("Try again!");
-            }
-            if (tries == 3) {
-                System.out.println("You Lost!");
-                break;
-            }
-        }
+        int startV = Integer.valueOf(scanner.nextLine());
+        int endV = Integer.valueOf(scanner.nextLine());
 
         scanner.close();
-
-        if (tries == 3) {
-            System.out.println("The correct answer was: " + result + "!");
-        } else {
-            if (tries == 1) {
-                System.out.println("You won the game in: " + tries + " try! Wow!");
-            } else {
-                System.out.println("You won the game in: " + tries + " tires!");
-            }
-        }
+        divisibleByThree(startV, endV);
     }
 }
