829315d744ea4705270acf8cef87cf6d127007b0
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-10-01 09:25:18 +0300

    30 character line

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -6,14 +6,21 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Input word: ");
-        String word = scanner.nextLine();
+        System.out.println("Input first word: ");
+        String word1 = scanner.nextLine();
+        System.out.println("Input second word: ");
+        String word2 = scanner.nextLine();
         System.out.println();
 
         scanner.close();
 
-        for (int i = 0; i < word.length(); i++) {
-            System.out.println(word);
+        int seperationLenght = 30 - word1.length() - word2.length();
+
+        System.out.print(word1);
+        for (int i = 0; i < seperationLenght; i++) {
+            System.out.print(".");
         }
+        System.out.print(word2);
     }
 }
+
