c3c920e3cc650d532556b1dd8b46489e63169d08
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-10-01 09:29:44 +0300

    word print vertical

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -6,21 +6,14 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Input first word: ");
+        System.out.println("Input word: ");
         String word1 = scanner.nextLine();
-        System.out.println("Input second word: ");
-        String word2 = scanner.nextLine();
-        System.out.println();
 
         scanner.close();
 
-        int seperationLenght = 30 - word1.length() - word2.length();
-
-        System.out.print(word1);
-        for (int i = 0; i < seperationLenght; i++) {
-            System.out.print(".");
+        for (int i = 0; i < word1.length(); i++) {
+            System.out.println(word1.charAt(i));
         }
-        System.out.print(word2);
     }
 }
 
