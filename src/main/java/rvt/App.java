a06350e1897d7a998c55c8edc447e9d8a37600e5
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-10-01 09:04:22 +0300

    sum of N

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -6,16 +6,18 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter start: ");
-        int startV = Integer.valueOf(scanner.nextLine());
-        System.out.println("\nEnter end: ");
-        int endV = Integer.valueOf(scanner.nextLine());
-        System.out.println();
+        System.out.println("Enter N: ");
+        int startN = Integer.valueOf(scanner.nextLine());
 
         scanner.close();
 
-        for (int i = startV; i < endV + 1; i++) {
-            System.out.println(i);          
+        int nSum = 0;
+
+        for (int i = 0; i < startN + 1; i++) {
+            nSum = nSum + i;         
         }
+
+        System.out.println("Sum = " + nSum);
+        System.out.println("Formula = " + (startN * (startN + 1)) / 2);
     }
 }
