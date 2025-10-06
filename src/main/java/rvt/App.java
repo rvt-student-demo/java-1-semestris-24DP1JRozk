8994c9e2a8925efdf3078305ce32ded84194eb98
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-10-06 10:22:48 +0300

    Ultimate mix of exercises from chapter 19

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -2,19 +2,15 @@ package rvt;
 
 import java.util.Scanner;
 
+import javax.naming.InitialContext;
+
 public class App {
     public static void ex1(int startV, int endV) {
         for (int i = startV; i < endV + 1; i++) {
             System.out.println(i);          
         }
     }
-    public static void ex2() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter N: ");
-        int startN = Integer.valueOf(scanner.nextLine());
-
-        scanner.close();
+    public static void ex2(int startN) {
 
         int nSum = 0;
 
@@ -25,13 +21,7 @@ public class App {
         System.out.println("Sum = " + nSum);
         System.out.println("Formula = " + (startN * (startN + 1)) / 2);
     }
-    public static void ex3() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter N: ");
-        int startN = Integer.valueOf(scanner.nextLine());
-
-        scanner.close();
+    public static void ex3(int startN) {
 
         int nSum = 0;
 
@@ -42,15 +32,7 @@ public class App {
         System.out.println("Sum = " + nSum);
         System.out.println("Formula = " + (startN * (startN + 1) * (2 * startN + 1)) / 6);
     }
-    public static void ex4() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter low: ");
-        int startV = Integer.valueOf(scanner.nextLine());
-        System.out.println("Enter high: ");
-        int endV = Integer.valueOf(scanner.nextLine());
-
-        scanner.close();
+    public static void ex4(int startV, int endV) {
 
         int intSum = 0;
 
@@ -60,29 +42,13 @@ public class App {
 
         System.out.println("Sum: " + intSum);
     }
-    public static void ex5() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Input word: ");
-        String word = scanner.nextLine();
-        System.out.println();
-
-        scanner.close();
+    public static void ex5(String word) {
 
         for (int i = 0; i < word.length(); i++) {
             System.out.println(word);
         }
     }
-    public static void ex6() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Input first word: ");
-        String word1 = scanner.nextLine();
-        System.out.println("Input second word: ");
-        String word2 = scanner.nextLine();
-        System.out.println();
-
-        scanner.close();
+    public static void ex6(String word1, String word2) {
 
         int seperationLenght = 30 - word1.length() - word2.length();
 
@@ -92,13 +58,7 @@ public class App {
         }
         System.out.print(word2);
     }
-    public static void ex7() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Input word: ");
-        String word1 = scanner.nextLine();
-
-        scanner.close();
+    public static void ex7(String word1) {
 
         for (int i = 0; i < word1.length(); i++) {
             System.out.println(word1.charAt(i));
@@ -106,47 +66,56 @@ public class App {
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Input start N:");
+        int startV = Integer.valueOf(scanner.nextLine());
+        System.out.println("Input end N:");
+        int endV = Integer.valueOf(scanner.nextLine()); 
+        System.out.println();
+        ex1(startV, endV);
 
-        while (true) {
+        System.out.println();
 
-            System.out.print("Input the exercise you want to run (from 1 to 7): ");
-            int exercise = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter N: ");
+        int startN = Integer.valueOf(scanner.nextLine());
+        ex2(startN);
 
-            if (exercise == 1) {
-                System.out.println("Exercise 1 — Run of Integers");
+        System.out.println();
 
-                System.out.println("Enter start: ");
-                int startV = Integer.valueOf(scanner.nextLine());
-                System.out.println("\nEnter end: ");
-                int endV = Integer.valueOf(scanner.nextLine());
-                System.out.println();
+        System.out.println("Enter N: ");
+        startN = Integer.valueOf(scanner.nextLine());
+        ex3(startN);
 
-                scanner.close();
-                
-                ex1(startV, endV);
-            } else if (exercise == 2) {
-                System.out.println("Exercise 2 — Sum of Sequential Integers");
-                ex2();
-            } else if (exercise == 3) {
-                System.out.println("Exercise 3 — Sum of Sequential Squares");
-                ex3();
-            } else if (exercise == 4) {
-                System.out.println("Exercise 4 — Sum of a Range of Sequential Integers");
-                ex4();
-            } else if (exercise == 5) {
-                System.out.println("Exercise 5 — Repeatedly Echo a Word");
-                ex5();
-            } else if (exercise == 6) {
-                System.out.println("Exercise 6 — Words Separated by Dots");
-                ex6();
-            } else if (exercise == 7) {
-                System.out.println("Exercise 7 — One Letter per Line");
-                ex7();
-            } else {
-                System.out.println("Incorrect Input!");
-            }
-            System.out.println();
-        }
+        System.out.println();
+
+        System.out.println("Enter low: ");
+        startV = Integer.valueOf(scanner.nextLine());
+        System.out.println("Enter high: ");
+        endV = Integer.valueOf(scanner.nextLine());
+        ex4(startV, endV);
+
+        System.out.println();
+
+        System.out.println("Input word: ");
+        String word = scanner.nextLine();
+        System.out.println();
+        ex5(word);
+
+        System.out.println();
+
+        System.out.println("Input first word: ");
+        String word1 = scanner.nextLine();
+        System.out.println("Input second word: ");
+        String word2 = scanner.nextLine();
+        System.out.println();
+        ex6(word1, word2);
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println("Input word: ");
+        word1 = scanner.nextLine();
+        ex7(word1);
     }
 }
 
