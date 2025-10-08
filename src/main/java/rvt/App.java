fd87c2fb930370e310d313a54c44e8d2ba18f0bb
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-10-08 09:19:08 +0300

    Print in stars

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -3,15 +3,16 @@ package rvt;
 public class App {
     public static void main(String[] args) {
         int[] arr = {5, 1, 3, 4, 2};
-        System.out.println(sumOfNumbersInArray(arr));
+        printStars(arr);
     }
 
-    public static int sumOfNumbersInArray(int[] array) {
-        int sumVar = 0;
+    public static void printStars(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            sumVar = sumVar + array[i];
+            for (int j = 0; j < array[i]; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
-        return sumVar;
     }
 }
 
