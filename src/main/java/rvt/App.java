ea92c2b93e968901953a66daa6aaeafd3ebe8360
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-10-08 09:14:30 +0300

    Array sum

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,30 +1,17 @@
 package rvt;
 
-import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) {
-        int[] arr = {1, 3, 5, 7, 9};
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        int[] arr = {5, 1, 3, 4, 2};
+        System.out.println(sumOfNumbersInArray(arr));
+    }
+
+    public static int sumOfNumbersInArray(int[] array) {
+        int sumVar = 0;
+        for (int i = 0; i < array.length; i++) {
+            sumVar = sumVar + array[i];
         }
-
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("\nWhitch two index values sould be swapped?");
-        int first_swap = Integer.valueOf(scanner.nextLine());
-        int second_swap = Integer.valueOf(scanner.nextLine());
-        System.out.println();
-
-        scanner.close();
-
-        int temp = arr[first_swap];
-        arr[first_swap] = arr[second_swap];
-        arr[second_swap] = temp;
-
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
-        }
+        return sumVar;
     }
 }
 
