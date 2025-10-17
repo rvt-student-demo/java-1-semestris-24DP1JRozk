ad9f9bf6d28ed5b8e5b364cf70f5c2d995997513
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-10-17 10:44:00 +0300

    Only These numbers

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -4,22 +4,29 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
 
-        String index = "";
+        String index = scanner.nextLine();
         while (true) {
-            index = scanner.nextLine();
-
             if (index == "") {
                 break;
             } else {
-                list.add(index);
+                list.add(Integer.valueOf(index));
             }
+            index = scanner.nextLine();
+        }
+
+        System.out.print("First: ");
+        Integer first = Integer.valueOf(scanner.nextLine());
+        System.out.print("Last: ");
+        Integer second = Integer.valueOf(scanner.nextLine());
+
+        for (int i = first; i < second + 1; i++) {
+            System.out.println(list.get(i));
         }
 
         scanner.close();
-        System.out.println("In total: " + list.size());
     }
 }
 
