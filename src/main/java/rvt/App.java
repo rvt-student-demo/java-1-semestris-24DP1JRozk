7e26246d755d460ee17cb782398cd61fab61aed1
Joel Rosehill <rozkalnsjoels@gmail.com>
2025-10-31 11:30:58 +0200

    4-1 Statistics part 1 and 2

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -3,20 +3,46 @@ import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
-        ArrayList<String> strings = new ArrayList<>();
-
-        strings.add("First");
-        strings.add("Second");
-        strings.add("Third");
-
-        System.out.println(strings);
-
-        removeLast(strings);
-
-        System.out.println(strings);
+        Statistics statistics = new Statistics();
+        statistics.addNumber(3);
+        statistics.addNumber(5);
+        statistics.addNumber(1);
+        statistics.addNumber(2);
+        System.out.println("Count: " + statistics.getCount());
+        System.out.println("Sum: " + statistics.sum());
+        System.out.println("Average: " + statistics.average());
     }
     public static void removeLast(ArrayList<String> list) {
         list.remove(list.size() - 1);
+    }
+    
+    public static class Statistics {
+        private int count;
+        private int sum;
+
+        public Statistics() {
+            this.count = 0;
+        }
+
+        public void addNumber(int number) {
+            this.count++;
+            this.sum += number;
+        }
+
+        public int getCount() {
+            return this.count;
+        }
+
+        public int sum() {
+            return this.sum;
+        }
+
+        public double average() {
+            if (this.count == 0) {
+                return 0;
+            }
+            return this.sum * 1.0 / this.count;
+        }
     }
 }
 
