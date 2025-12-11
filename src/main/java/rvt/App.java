2e7c29c1b185dce6ffa36f01df2d77a89ca725f9
Joels <rozkalnsjoels@gmail.com>
2025-12-11 21:23:06 +0200

    chapter 9-1 part 5

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,4 +1,5 @@
 package rvt;    
+import java.util.ArrayList;
 
 
 public class App {
@@ -65,22 +66,18 @@ public class App {
         }
     }
 
+    public static void printPersons(ArrayList<Person> persons) {
+        for (Person p : persons) {
+            System.out.println(p);
+        }
+    }
+
     public static void main(String[] args) {
 
-        Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
-        Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
+        ArrayList<Person> persons = new ArrayList<>();
+        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
+        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
 
-        System.out.println(ada);
-        System.out.println(esko);
-
-        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
-
-        int i = 0;
-        while (i < 25) {
-            ollie.study();
-            i++;
-        }
-
-        System.out.println(ollie);
+        printPersons(persons);
     }
 }
