64f8c407e23e6dcc899ac7b1e69694a681f5d028
Joels <rozkalnsjoels@gmail.com>
2025-12-11 21:21:47 +0200

    chapter 9-1 part 4

--- src/main/java/rvt/App.java
+++ src/main/java/rvt/App.java
@@ -1,4 +1,4 @@
-package rvt;
+package rvt;    
 
 
 public class App {
@@ -50,16 +50,36 @@ public class App {
         }
     }
 
+    static class Teacher extends Person {
+
+        private final int salary;
+
+        public Teacher(String name, String address, int salary) {
+            super(name, address);
+            this.salary = salary;
+        }
+
+        @Override
+        public String toString() {
+            return getName() + "\n  " + getAddress() + "\n  salary " + salary + " euro/month";
+        }
+    }
+
     public static void main(String[] args) {
 
-        Student ollie = new Student(
-            "Ollie",
-            "6381 Hollywood Blvd. Los Angeles 90028"
-        );
+        Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
+        Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
 
-        System.out.println(ollie);
+        System.out.println(ada);
+        System.out.println(esko);
 
-        ollie.study();
+        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+
+        int i = 0;
+        while (i < 25) {
+            ollie.study();
+            i++;
+        }
 
         System.out.println(ollie);
     }
